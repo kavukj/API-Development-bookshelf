21d57bbbaa9778fb2272f9431f077195c5efc4dc
Kavya <jainkavya6890@gmail.com>
2022-08-14 00:09:16 +0100

    Basic setup

---
+++ Project/backend/flaskr/__init__.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy  # , or_
+from flask_cors import CORS
+import random
+
+from models import setup_db, Book
+
+BOOKS_PER_SHELF = 8
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    # CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+        )
+        return response
+
+    return app
